950bb58c4a9b8a3dab0be3237f5cdff520496597
LOIC GAILLARD <loic.gaillard@etu.univ-lyon1.fr>
2020-02-17 14:20:43 +0100

    ajout de quelques infos sur le readme

---
+++ README.md
@@ -0,0 +1,21 @@
+# TestVUEjs
+Test pour s'entrainer au vuejs
+
+## Temps passé à l'apprentissage de vue.JS
+4 heures de tutoriels et de recherche via la documentation officielle et open classroom (partie 1 et 3)
+https://openclassrooms.com/en/courses/5664336-create-a-web-application-with-vue-js
+
+## Temps passé à l'apprentissage d'AJAX et JSON
+3 heures via la documentation officielle / le cours d'un de mes professeurs : 
+https://perso.liris.cnrs.fr/pierre-antoine.champin/enseignement/intro-js/s3.html
+
+## Temps passé au développement / Test des fonctionnalités
+2 heures
+
+# Total du temps passé sur ce projet :
+~11h
+
+### Notes pour AJAX (mémo)
+Ajax permet de créer une interface fonctionnant sur un navigateur avec le langage JavaScript
+On utilise PHP pour : consulter une base de donnée et la metttre à jour avec les données entrées par l'interface, et il exécute les traitements demandés par l'utilisateur qui requièrent des données stockées sur le serveur.
+On envoie des données à un script PHP qui les écrit dans un fichier 
---
+++ php/script.php
@@ -0,0 +1,56 @@
+<?php
+//Déclaration des données statiques 
+
+$pheonixSong = array(
+	"image"=>"no file",
+	"title"=>"Lisztomania",
+	"artist"=>"Phoenix",
+	"listen"=>false,
+	"stop"=>false,
+	"link"=>"musics/phoenix-lisztomania.mp3"
+);
+$dragonsSong = array(
+	"image"=>"no file",
+	"title"=>"Birds",
+	"artist"=>"Imagine Dragons",
+	"listen"=>false,
+	"stop"=>false,
+	"link"=>"musics/imagine-dragons-birds.mp3"
+);
+$punkSong = array(
+	"image"=>"no file",
+	"title"=>"Get Lucky",
+	"artist"=>"Daft Punk",
+	"listen"=>false,
+	"stop"=>false,
+	"link"=>"musics/daft-punk-get-lucky.mp3"
+);
+$justiceSong = array(
+	"image"=>"no file",
+	"title"=>"D.A.N.C.E",
+	"artist"=>"Justice",
+	"listen"=>false,
+	"stop"=>false,
+	"link"=>"musics/justice-dance.mp3"
+);
+$take = array(
+	"image"=>"no file",
+	"title"=>"Take on me",
+	"artist"=>"Aha",
+	"listen"=>false,
+	"stop"=>false,
+	"link"=>"musics/justice-dance.mp3"
+);
+
+//Ajout des données statiques dans un tableaux pour pouvoir l'écrire sous forme d'un JSON
+$data = array(
+            "songs" => array(
+            	$pheonixSong,
+            	$dragonsSong,
+            	$punkSong,
+            	$justiceSong,
+            	$take)
+        );
+
+echo json_encode($data);
+?>
